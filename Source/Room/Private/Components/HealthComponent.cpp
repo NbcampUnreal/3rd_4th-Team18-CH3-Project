b2d8ec4ebbdeefefa6e57cc45649b0d7fc78d50b
yh1457 <gyh1457@gmail.com>
2025-07-29 10:19:56 +0900

    <Add> HealthComponent
    HealthComponent Is Wirrten.

---
+++ Source/Room/Private/Components/HealthComponent.cpp
@@ -0,0 +1,76 @@
+#include "Components/HealthComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Character.h"
+
+UHealthComponent::UHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UHealthComponent::SetDestroyDelay(float NewDelay)
+{
+	if (NewDelay<=0.0f) NewDelay = 1.0f;
+	DestroyDelay = NewDelay;
+}
+
+void UHealthComponent::SetMaxHealth(float NewMaxHealth)
+{
+	if (NewMaxHealth<=0.0f) NewMaxHealth = 1.0f;
+	MaxHealth = NewMaxHealth;
+}
+
+float UHealthComponent::GetDestroyDelay() const
+{
+	return DestroyDelay;	
+}
+
+float UHealthComponent::GetMaxHealth() const
+{
+	return MaxHealth;
+}
+
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CharacterRef = Cast<ACharacter>(GetOwner());
+	if (!CharacterRef) return;
+	
+	CurrentHealth = MaxHealth;
+	CharacterRef->OnTakeAnyDamage.AddDynamic(this,&ThisClass::DamageTaken);
+}
+
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UHealthComponent::DamageTaken(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser)
+{
+	if (DamagedActor != CharacterRef) return;
+
+	SetCurrentHealth(CurrentHealth - Damage);
+}
+
+
+void UHealthComponent::SetCurrentHealth(float NewHealth)
+{
+	CurrentHealth = NewHealth;
+	CurrentHealth = FMath::Clamp(CurrentHealth, 0.0f, MaxHealth);
+
+	if (CurrentHealth <= 0.0f)
+	{
+		OnDeath();
+	}
+}
+
+void UHealthComponent::OnDeath()
+{
+	if (!CharacterRef) return;
+	CharacterRef->GetController()->UnPossess();
+	CharacterRef->GetMesh()->SetSimulatePhysics(true);
+	CharacterRef->GetMesh()->SetCollisionProfileName(TEXT("Ragdoll"));
+	CharacterRef->GetCapsuleComponent()->SetCollisionProfileName(TEXT("Spectator"));
+	CharacterRef->SetLifeSpan(DestroyDelay);
+}
+
---
+++ Source/Room/Public/Components/HealthComponent.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+class ACharacter;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ROOM_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UHealthComponent();
+
+	void SetDestroyDelay(float NewDelay);
+	void SetMaxHealth(float NewMaxHealth);
+
+	float GetDestroyDelay()const;
+	float GetMaxHealth()const;
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY()
+	TObjectPtr<ACharacter> CharacterRef = nullptr;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "Health")
+	float CurrentHealth = 100.0;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "Health")
+	float MaxHealth = 100.0;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "Health")
+	float DestroyDelay = 5.0f;
+	
+private:
+	UFUNCTION()
+	void DamageTaken(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser);
+	void SetCurrentHealth(float NewHealth);
+	void OnDeath();
+
+
+};
