1d5e86c4461f971377579020df0748d036461f97
piecebypiece <chlx357@gmail.com>
2025-07-30 13:34:47 +0900

    Update GamePlayTas 모듈 추가

--- Source/Room/Room.Build.cs
+++ Source/Room/Room.Build.cs
@@ -19,6 +19,7 @@ public class Room : ModuleRules
 			"NavigationSystem",
 			"GameplayTasks",
 			"EngineSettings",
+			"GameplayTags",
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
