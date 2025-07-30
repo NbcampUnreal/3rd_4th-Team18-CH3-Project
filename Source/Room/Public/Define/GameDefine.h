099e01f6540e3e529fef2f9d23a6e34fd2ebc5ef
piecebypiece <chlx357@gmail.com>
2025-07-30 15:58:11 +0900

    Add GamePlayTag
    플레이어와 적군용 GamePlayTag 추가.

---
+++ Source/Room/Public/Define/GameDefine.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+// 게임에서 필요하며 전역적으로 사용할 리터럴 값 정의장소
+namespace GameDefine
+{
+	const FGameplayTag PlayerTag = FGameplayTag::RequestGameplayTag(TEXT("Actor.Player"));
+	const FGameplayTag EnemyTag = FGameplayTag::RequestGameplayTag(TEXT("Actor.Enemy"));
+}
