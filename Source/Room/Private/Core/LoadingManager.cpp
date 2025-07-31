8a20a09be0f1ebf79bd2f9c68cff0a1a10463d37
piecebypiece <chlx357@gmail.com>
2025-07-31 11:58:22 +0900

    Update LoadingManager 에셋 레퍼런스 카운트 추가 SoftObjectPtr을 쉽게 사용할 수 있는  함수 추가

--- Source/Room/Private/Core/LoadingManager.cpp
+++ Source/Room/Private/Core/LoadingManager.cpp
@@ -10,7 +10,8 @@ void ULoadingManager::RequestLoad(const TSoftObjectPtr<UObject>& Asset, FOnAsset
     }
 
     FSoftObjectPath Path = Asset.ToSoftObjectPath();
-
+    AssetRefCount.FindOrAdd(Path)++;
+    
     if (LoadedAssets.Contains(Path))
     {
         // 이미 로드됨
@@ -51,6 +52,7 @@ void ULoadingManager::RequestLoadBatch(const TArray<TSoftObjectPtr<UObject>>& As
             continue;
 
         FSoftObjectPath Path = Asset.ToSoftObjectPath();
+        AssetRefCount.FindOrAdd(Path)++;
 
         if (!LoadedAssets.Contains(Path) && !ActiveHandles.Contains(Path))
         {
@@ -87,19 +89,58 @@ void ULoadingManager::RequestUnload(const TSoftObjectPtr<UObject>& Asset)
 {
     FSoftObjectPath Path = Asset.ToSoftObjectPath();
 
-    if (LoadedAssets.Contains(Path))
+    if (int32* Count = AssetRefCount.Find(Path))
     {
-        StreamableManager.Unload(Path);
-        LoadedAssets.Remove(Path);
-    }
-
-    if (ActiveHandles.Contains(Path))
-    {
-        ActiveHandles.Remove(Path);
+        (*Count)--;
+        if (*Count <= 0)
+        {
+            AssetRefCount.Remove(Path);
+            if (LoadedAssets.Contains(Path))
+            {
+                StreamableManager.Unload(Path);
+                LoadedAssets.Remove(Path);
+            }
+        }
     }
 }
 
 bool ULoadingManager::IsLoaded(const TSoftObjectPtr<UObject>& Asset) const
 {
     return LoadedAssets.Contains(Asset.ToSoftObjectPath());
+}
+
+void ULoadingManager::UnloadUnusedAssets(const TSet<FSoftObjectPath>& KeepList)
+{
+    for (auto It = AssetRefCount.CreateIterator(); It; ++It)
+    {
+        if (!KeepList.Contains(It->Key))
+        {
+            StreamableManager.Unload(It->Key);
+            LoadedAssets.Remove(It->Key);
+            It.RemoveCurrent();
+        }
+    }
+    CollectGarbage(GARBAGE_COLLECTION_KEEPFLAGS);
+}
+
+void ULoadingManager::GetOrLoadAsset(const TSoftObjectPtr<UObject>& Asset, FOnAssetReady OnReady)
+{
+    if (Asset.IsValid())
+    {
+        // 이미 메모리에 있음 → 바로 반환
+        OnReady.ExecuteIfBound(Asset.Get());
+        return;
+    }
+
+    if (Asset.ToSoftObjectPath().IsNull())
+    {
+        OnReady.ExecuteIfBound(nullptr);
+        return;
+    }
+
+    // 비동기 로드
+    RequestLoad(Asset, FOnAssetLoadComplete::CreateLambda([Asset, OnReady]()
+    {
+        OnReady.ExecuteIfBound(Asset.Get());
+    }));
 }
--- Source/Room/Public/Core/LoadingManager.h
+++ Source/Room/Public/Core/LoadingManager.h
@@ -8,6 +8,8 @@
 
 DECLARE_DELEGATE(FOnAssetLoadComplete)
 
+// UObject* 를 전달하는 콜백
+DECLARE_DELEGATE_OneParam(FOnAssetReady, UObject*);
 UCLASS()
 class ROOM_API ULoadingManager : public UGameInstanceSubsystem
 {
@@ -19,7 +21,9 @@ public:
 
     void RequestUnload(const TSoftObjectPtr<UObject>& Asset);
     bool IsLoaded(const TSoftObjectPtr<UObject>& Asset) const; 
-
+    void UnloadUnusedAssets(const TSet<FSoftObjectPath>& KeepList);
+    // 새로 추가: 로드돼 있으면 즉시, 아니면 로드 후 콜백
+    void GetOrLoadAsset(const TSoftObjectPtr<UObject>& Asset, FOnAssetReady OnReady);
 private:
     FStreamableManager StreamableManager;
 
@@ -28,5 +32,7 @@ private:
 
     // 이미 로드 완료된 오브젝트 목록
     TSet<FSoftObjectPath> LoadedAssets;
+    
+    TMap<FSoftObjectPath, int32> AssetRefCount;
 };
 
