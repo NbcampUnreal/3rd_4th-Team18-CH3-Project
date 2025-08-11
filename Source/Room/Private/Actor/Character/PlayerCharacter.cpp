2d44e02ca8ffd44368c730b19efd5c9bc359880e
jin945 <wlsrnjs945@naver.com>
2025-08-11 10:14:24 +0900

    Revert "update PlayerCharacter"
    This reverts commit 94eb132934e6a8e1f47539c1452d9138359703f8.

--- Source/Room/Private/Actor/Character/PlayerCharacter.cpp
+++ Source/Room/Private/Actor/Character/PlayerCharacter.cpp
@@ -4,15 +4,14 @@ APlayerCharacter::APlayerCharacter()
 {
 	ActorTag = GameDefine::PlayerTag;
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
-	SpringArm->SetupAttachment(RootComponent); 
+	SpringArm->SetupAttachment(GetCapsuleComponent()); 
 	
-	SpringArm->TargetArmLength = 300.0f; 
-    SpringArm->bUsePawnControlRotation = true;
-	SpringArm->SocketOffset = FVector(0.0f, 100.0f, 0.0f);
+	SpringArm->TargetArmLength = 400.0f; 
+    
+
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
-	Camera->bUsePawnControlRotation = false;
-	
+
 	PlayerAttackComponent = CreateDefaultSubobject<UPlayerAttackComponent>(TEXT("PlayerAttackComponent"));
 	PlayerAttackComponent->SetupAttachment(GetMesh(), FName("Muzzle"));
 
