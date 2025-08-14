391ae6fc6d44d338cbf36c638753044157ddc71c
piecebypiece <chlx357@gmail.com>
2025-08-14 17:18:12 +0900

    Add 레벨 연결 정보용 액터 생성

---
+++ Source/Room/Private/Actor/LevelConnector.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/LevelConnector.h"
+#include UE_INLINE_GENERATED_CPP_BY_NAME(LevelConnector) 
+
+// Sets default values
+ALevelConnector::ALevelConnector()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+}
